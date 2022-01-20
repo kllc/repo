26b04801fb17ccbb17ecdab8516e7e02f83628f5
K <kijima@kllc.jp>
2022-01-20 15:10:20 +0900

    test

--- script/test.js
+++ script/test.js
@@ -1,25 +1,44 @@
+// (function (root, factory) {
+//   if (typeof define === "function" && define.amd) {
+//     // AMD
+//     define(["myModule", "myOtherModule"], factory);
+//   } else if (typeof exports === "object") {
+//     // CommonJS
+//     module.exports = factory(require("myModule"), require("myOtherModule"));
+//   } else {
+//     // Browser globals (Note: root is window)
+//     root.returnExports = factory(root.myModule, root.myOtherModule);
+//   }
+// })(this, function (myModule, myOtherModule) {
+//   // Methods
+//   function notHelloOrGoodbye() {} // A private method
+//   function hello() {
+//     alert(1);
+//   } // A public method because it's returned (see below)
+//   function goodbye() {} // A public method because it's returned (see below)
+
+//   // Exposed public methods
+//   return {
+//     Hello: hello,
+//     Goodbye: goodbye,
+//   };
+// });
+
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD
-    define(["myModule", "myOtherModule"], factory);
+    define(["jquery"], factory);
   } else if (typeof exports === "object") {
-    // CommonJS
-    module.exports = factory(require("myModule"), require("myOtherModule"));
+    // Node, CommonJS-like
+    module.exports = factory(require("jquery"));
   } else {
-    // Browser globals (Note: root is window)
-    root.returnExports = factory(root.myModule, root.myOtherModule);
+    // Browser globals (root is window)
+    root.returnExports = factory(root.jQuery);
   }
-})(this, function (myModule, myOtherModule) {
-  // Methods
-  function notHelloOrGoodbye() {} // A private method
-  function hello() {
-    alert(1);
-  } // A public method because it's returned (see below)
-  function goodbye() {} // A public method because it's returned (see below)
+})(this, function ($) {
+  //    methods
+  function myFunc() {}
 
-  // Exposed public methods
-  return {
-    Hello: hello,
-    Goodbye: goodbye,
-  };
+  //    exposed public method
+  return myFunc;
 });
