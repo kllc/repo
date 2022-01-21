4ac96c034fe80daed7d673733bf4b89405bd4fc2
K <kijima@kllc.jp>
2022-01-21 19:34:22 +0900

    test

--- script/test.js
+++ script/test.js
@@ -1,44 +1,25 @@
-// (function (root, factory) {
-//   if (typeof define === "function" && define.amd) {
-//     // AMD
-//     define(["myModule", "myOtherModule"], factory);
-//   } else if (typeof exports === "object") {
-//     // CommonJS
-//     module.exports = factory(require("myModule"), require("myOtherModule"));
-//   } else {
-//     // Browser globals (Note: root is window)
-//     root.returnExports = factory(root.myModule, root.myOtherModule);
-//   }
-// })(this, function (myModule, myOtherModule) {
-//   // Methods
-//   function notHelloOrGoodbye() {} // A private method
-//   function hello() {
-//     alert(1);
-//   } // A public method because it's returned (see below)
-//   function goodbye() {} // A public method because it's returned (see below)
-
-//   // Exposed public methods
-//   return {
-//     Hello: hello,
-//     Goodbye: goodbye,
-//   };
-// });
-
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD
-    define(["jquery"], factory);
+    define(["myModule", "myOtherModule"], factory);
   } else if (typeof exports === "object") {
-    // Node, CommonJS-like
-    module.exports = factory(require("jquery"));
+    // CommonJS
+    module.exports = factory(require("myModule"), require("myOtherModule"));
   } else {
-    // Browser globals (root is window)
-    root.returnExports = factory(root.jQuery);
+    // Browser globals (Note: root is window)
+    root.returnExports = factory(root.myModule, root.myOtherModule);
   }
-})(this, function ($) {
-  //    methods
-  function myFunc() {}
+})(this, function (myModule, myOtherModule) {
+  // Methods
+  function notHelloOrGoodbye() {} // A private method
+  function hello() {
+    alert(1);
+  } // A public method because it's returned (see below)
+  function goodbye() {} // A public method because it's returned (see below)
 
-  //    exposed public method
-  return myFunc;
+  // Exposed public methods
+  return {
+    Hello: hello,
+    Goodbye: goodbye,
+  };
 });
