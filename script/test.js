1c5087e4e861663bbc5cdaceb65385ee582c2085
K <kijima@kllc.jp>
2022-01-24 20:23:30 +0900

    adb2cal

--- script/test.js
+++ script/test.js
@@ -1,25 +1,28 @@
-(function (root, factory) {
-  if (typeof define === "function" && define.amd) {
-    // AMD
-    define(["myModule", "myOtherModule"], factory);
-  } else if (typeof exports === "object") {
-    // CommonJS
-    module.exports = factory(require("myModule"), require("myOtherModule"));
-  } else {
-    // Browser globals (Note: root is window)
-    root.returnExports = factory(root.myModule, root.myOtherModule);
-  }
-})(this, function (myModule, myOtherModule) {
-  // Methods
-  function notHelloOrGoodbye() {} // A private method
-  function hello() {
-    alert(1);
-  } // A public method because it's returned (see below)
-  function goodbye() {} // A public method because it's returned (see below)
+// (function (root, factory) {
+//   if (typeof define === "function" && define.amd) {
+//     // AMD
+//     define(["myModule", "myOtherModule"], factory);
+//   } else if (typeof exports === "object") {
+//     // CommonJS
+//     module.exports = factory(require("myModule"), require("myOtherModule"));
+//   } else {
+//     // Browser globals (Note: root is window)
+//     root.returnExports = factory(root.myModule, root.myOtherModule);
+//   }
+// })(this, function (myModule, myOtherModule) {
+//   // Methods
+//   function notHelloOrGoodbye() {} // A private method
+//   function hello() {
+//     alert(1);
+//   } // A public method because it's returned (see below)
+//   function goodbye() {} // A public method because it's returned (see below)
 
-  // Exposed public methods
-  return hello;
-});
+//   // Exposed public methods
+//   return {
+//     hello: hello,
+//     goodbye: goodbye,
+//   };
+// });
 
 // (function (global, factory) {
 //   typeof exports === "object" && typeof module !== "undefined"
