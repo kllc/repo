7972995050c918180e7df35406bad0dcc8db59f6
K <kijima@kllc.jp>
2022-07-30 22:27:36 +0900

    ad test

--- script/livelpad.js
+++ script/livelpad.js
@@ -1,6 +1,12 @@
 export default (plan) => {
   if (plan) {
-    return 'Powered by <a href="https://livelp.net" target="_blank">Livelp.net</a>';
+    switch (plan) {
+      case "test":
+        return 'Powered by <a href="https://livelp.net" target="_blank">Livelp</a>';
+      default:
+        return 'Powered by <a href="https://livelp.net" target="_blank">Livelp.net</a>';
+        break;
+    }
   } else {
     return "";
   }
